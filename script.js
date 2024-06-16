bc0a6ab7112e280845869b39ec2b6451e48504da
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-06-16 22:16:09 +0600

    half hero section animatin done

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 const cursorAnimation = () => {
 
     let cursor = document.getElementById('cursor');
+    let h1Tags = document.querySelectorAll('h1');
+    let h3Tags = document.querySelectorAll('h3');
+    let h5Tags = document.querySelectorAll('h5');
+    let aTags = document.querySelectorAll('a');
+    let pTags = document.querySelectorAll('p');
+    let iTags = document.querySelectorAll('i');
 
-    document.querySelector('body').addEventListener('mousemove', function (event) {
+    window.addEventListener('mousemove', function (event) {
 
         let x = event.clientX;
         let y = event.clientY;
@@ -17,31 +28,77 @@ const cursorAnimation = () => {
         cursor.classList.remove('bg-slate-100');
         cursor.classList.add('backdrop-invert');
     });
+
+
+    h1Tags.forEach(h1Tag => {
+        h1Tag.addEventListener('mouseenter', function () {
+            cursor.style.transform = 'scale(2)';
+        });
+
+        h1Tag.addEventListener('mouseleave', function () {
+            cursor.style.transform = 'scale(1)';
+        })
+    });
 }
 
 cursorAnimation();
 
 let time = document.getElementById('time');
 
-setInterval(function showTime() {
+function showTime() {
+    let date = new Date();
 
-    const date = new Date();
+    let hoursNow = date.getHours();
+    let minutesNow = date.getMinutes();
+    let secondsNow = date.getSeconds();
 
-    const hoursNow = date.getHours();
+    let isMorning = hoursNow < 12;
 
-    const minutesNow = date.getMinutes();
+    if (hoursNow > 12) {
+        hoursNow = hoursNow - 12;
+    } else if (hoursNow === 0) {
+        hoursNow = 12;
+    }
 
-    const secondsNow = date.getSeconds();
+    time.textContent = `${hoursNow.toString().padStart(2, '0')}:${minutesNow.toString().padStart(2, '0')}:${secondsNow.toString().padStart(2, '0')} ${isMorning ? "AM" : "PM"} EST`;
 
-    time.textContent = `${hoursNow.toString().padStart(2, '0')}:${minutesNow.toString().padStart(2, '0')}:${secondsNow.toString().padStart(2, '0')} ${isMorning === true ? "AM" : "PM"} EST`;
-
-    // console.log(hoursNow, ':', minutesNow, ':', secondsNow, isMorning === true ? "AM" : "PM");
-
-}, 1000);
-
-let isMorning;
-
-if (this.hoursNow > 12) {
-    hoursNow -= hoursNow - 12;
-    isMorning = true;
+    console.log(hoursNow, ':', minutesNow, ':', secondsNow, isMorning ? "AM" : "PM");
 }
+
+setInterval(showTime, 1000);
+
+showTime();
+
+function navbarAnimation() {
+    gsap.from(document.querySelector('#hero-section nav'), {
+        delay: 0.5,
+        y: -50,
+        opacity: 0,
+        duration: 1.2
+    });
+}
+
+navbarAnimation();
+
+const tl = gsap.timeline();
+
+tl.to(document.querySelectorAll('#hero-heading h1'), {
+    delay: 0.7,
+    y: 0,
+    duration: 1,
+    stagger: 0.2
+})
+    .to(document.querySelector('#hero-heading h5'), {
+        y: 0,
+        delay: -0.5
+    })
+    .to(document.querySelectorAll('#hero-meeting h3'), {
+        y: 0,
+        delay: -0.2,
+        stagger: 0.2
+    })
+    .from(document.querySelector('#hero-footer'), {
+        opacity: 0,
+        delay: -0.1,
+        stagger: 0.2
+    })
